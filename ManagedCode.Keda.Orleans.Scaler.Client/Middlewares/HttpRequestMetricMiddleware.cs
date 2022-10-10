a708ea03abbafa9479e783e1501e02217019e2b4
ksemenenko <mail@ksemenenko.com>
2022-10-10 11:58:20 +0200

    nuget

--- ManagedCode.Keda.Orleans.Scaler.Client/Middlewares/HttpRequestMetricMiddleware.cs
+++ ManagedCode.Keda.Orleans.Scaler.Client/Middlewares/HttpRequestMetricMiddleware.cs
@@ -1,25 +1,46 @@
 using ManagedCode.Keda.Orleans.Interfaces;
+using ManagedCode.TimeSeries.Summers;
 using Microsoft.AspNetCore.Http.Extensions;
 using Orleans;
 
 namespace ManagedCode.Keda.Orleans.Scaler.Client.Middlewares;
 
-public class HttpRequestMetricMiddleware
+public class HttpRequestMetricMiddleware : IDisposable
 {
     private readonly RequestDelegate _next;
     private readonly IClusterClient _clusterClient;
     private readonly ILogger<HttpRequestMetricMiddleware> _logger;
+    private PeriodicTimer _timer;
+    private Task _timerTask;
+    private CancellationTokenSource _token;
+    private readonly IntTimeSeriesSummer _summer = new(TimeSpan.FromSeconds(1), 30);
 
     public HttpRequestMetricMiddleware(ILogger<HttpRequestMetricMiddleware> logger, IClusterClient clusterClient, RequestDelegate next)
     {
         _logger = logger;
         _clusterClient = clusterClient;
         _next = next;
+        
+        _timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
+        _timerTask = Task.Run(async () =>
+        {
+            while (await _timer.WaitForNextTickAsync(_token.Token))
+            {
+                var requests = _summer.Samples.Average(s => s.Value);
+                _logger.LogInformation($"HOSTNAME:{Environment.GetEnvironmentVariable("HOSTNAME")};MachineName:{Environment.MachineName};\n  Requests Number: {requests}");
+            }
+        });
     }
 
     public async Task Invoke(HttpContext httpContext)
     {
+        _summer.Increment();
         _clusterClient.GetGrain<IRequestTrackerGrain>(0).TrackRequest().Ignore();
         await _next(httpContext);
+    }
+
+    public void Dispose()
+    {
+        _token.Cancel();
     }
 }
--- ManagedCode.Keda.Orleans.Scaler.Client/Middlewares/SignalRMonitorMiddleware.cs
+++ ManagedCode.Keda.Orleans.Scaler.Client/Middlewares/SignalRMonitorMiddleware.cs
@@ -4,27 +4,47 @@ using Orleans;
 
 namespace ManagedCode.Keda.Orleans.Scaler.Client.Middlewares;
 
-public class SignalRMonitorMiddleware : IHubFilter
+public class SignalRMonitorMiddleware : IHubFilter, IDisposable
 {
     private readonly IClusterClient _clusterClient;
 
     private readonly ILogger<SignalRMonitorMiddleware> _logger;
-
+    private PeriodicTimer _timer;
+    private Task _timerTask;
+    private CancellationTokenSource _token;
+    private volatile int _connections;
+    
     public SignalRMonitorMiddleware(ILogger<SignalRMonitorMiddleware> logger, IClusterClient clusterClient)
     {
         _logger = logger;
         _clusterClient = clusterClient;
+        _timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
+        _token = new();
+        _timerTask = Task.Run(async () =>
+        {
+            while (await _timer.WaitForNextTickAsync(_token.Token))
+            {
+                _logger.LogInformation($"HOSTNAME:{Environment.GetEnvironmentVariable("HOSTNAME")};MachineName:{Environment.MachineName};\n  Singal Connetions: {_connections}");
+            }
+        });
     }
 
     public Task OnConnectedAsync(HubLifetimeContext context, Func<HubLifetimeContext, Task> next)
     {
+        Interlocked.Increment(ref _connections);
         _clusterClient.GetGrain<ISignalRTrackerGrain>(0).OnConnectedAsync().Ignore();
         return next(context);
     }
 
     public Task OnDisconnectedAsync(HubLifetimeContext context, Exception exception, Func<HubLifetimeContext, Exception, Task> next)
     {
+        Interlocked.Decrement(ref _connections);
         _clusterClient.GetGrain<ISignalRTrackerGrain>(0).OnDisconnectedAsync().Ignore();
         return next(context, exception);
+    }
+
+    public void Dispose()
+    {
+        _token.Cancel();
     }
 }
